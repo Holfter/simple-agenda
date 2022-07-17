d57cd9b457b2313f83f2b7ad01c9ae650125f3f3
Matheus  Holfter <matheus.holfter@aldesenvolvimento.com.br>
2022-07-17 20:16:56 -0300

    Creating connection with database

---
+++ config/connection.php
@@ -0,0 +1,18 @@
+<?php 
+
+    $host = 'db';
+    $dbname = "agenda";
+    $user = 'root';
+    $pass = "secret";
+
+    try{
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+
+        // Turn on errors mode
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    }catch(PDOException $e){
+        // connection error
+        $error = $e->getMessage();
+        echo "Error: $error";
+    }
