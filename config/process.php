6bf21f421f693db49ea0cfb4bb5ad705e5b1c437
Matheus  Holfter <matheus.holfter@aldesenvolvimento.com.br>
2022-07-24 19:19:50 -0300

    contact editing functionality

--- config/process.php
+++ config/process.php
@@ -8,30 +8,59 @@
     $data = $_POST;
 
     if(!empty($data)){
-        $name = $data['name'];
-        $phone = $data['phone'];
-        $observations = $data['observations'];
-
-        $query = 'INSERT INTO contacts (name, phone, observations) VALUES (:name, :phone, :observations)';
-
-        $stmt = $conn->prepare($query);
-
-        $stmt->bindParam(':name', $name);
-        $stmt->bindParam(':phone', $phone);
-        $stmt->bindParam(':observations', $observations);
-
-        try{
-            $stmt->execute();
-            $_SESSION['msg'] = 'Contact successfully created';
+        // Create contact
+        if($data["type"] === "create") {
+            $name = $data['name'];
+            $phone = $data['phone'];
+            $observations = $data['observations'];
     
-        }catch(PDOException $e){
-            // connection error
-            $error = $e->getMessage();
-            echo "Error: $error";
+            $query = 'INSERT INTO contacts (name, phone, observations) VALUES (:name, :phone, :observations)';
+    
+            $stmt = $conn->prepare($query);
+    
+            $stmt->bindParam(':name', $name);
+            $stmt->bindParam(':phone', $phone);
+            $stmt->bindParam(':observations', $observations);
+    
+            try{
+                $stmt->execute();
+                $_SESSION['msg'] = 'Contact successfully created';
+        
+            }catch(PDOException $e){
+                // connection error
+                $error = $e->getMessage();
+                echo "Error: $error";
+            }
+            
+        }else if($data["type"] === "edit") {
+            $name = $data["name"];
+            $phone = $data["phone"];
+            $observations = $data["observations"];
+            $id = $data["id"];
+
+            $query = 'UPDATE contacts
+                      SET name = :name, phone = :phone, observations = :observations
+                      WHERE id = :id';
+            $stmt = $conn->prepare($query);
+
+            $stmt->bindParam(":name", $name);
+            $stmt->bindParam(":phone", $phone);
+            $stmt->bindParam(":observations", $observations);
+            $stmt->bindParam(":id", $id);
+
+            try{
+                $stmt->execute();
+                $_SESSION['msg'] = 'Contact successfully updated';
+        
+            }catch(PDOException $e){
+                // connection error
+                $error = $e->getMessage();
+                echo "Error: $error";
+            }
         }
-
+        
+        // Redirect user
         header('Location:' . "http://" . $_SERVER['SERVER_NAME']);
-
     }else{
         $id;
     
