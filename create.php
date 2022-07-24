3ebd532d21d054b1cd953a433381966420ce1569
Matheus  Holfter <matheus.holfter@aldesenvolvimento.com.br>
2022-07-24 17:22:19 -0300

    Create contact screen added

--- create.php
+++ create.php
@@ -2,7 +2,26 @@
     include_once('templates/header.php');
 ?>
 
-    <h1>Create contact</h1>    
+    <div class="container">
+        <?php include_once("templates/backbtn.html");?>
+        <h1 id="main-title">Create contact</h1>
+        <form id="create-form" action="<?= $BASE_URL ?>/config/process.php" methos="POST">
+            <input type="hidden" name="type" value="create">
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Name" required>
+            </div>
+            <div class="form-group">
+                <label for="name">Phone number:</label>
+                <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone number" required>
+            </div>
+            <div class="form-group">
+                <label for="name">Observations:</label>
+                <textarea type="text" class="form-control" id="observations" name="observations" placeholder="Observations" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Create</button>
+        </form>
+    </div>  
 
 <?php
     include_once('templates/footer.php');
--- templates/header.php
+++ templates/header.php
@@ -36,19 +36,19 @@
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="<?= $BASE_URL ?>/index.php">
-                <img src="<?= $BASE_URL ?>/img/logo.svg" alt="Agenda">
-            </a>
-            <div>
-                <div class="navbar-nav">
-                    <a href="<?= $BASE_URL ?>/index.php" class="nav-link active" id="home-link">
-                        Agenda
-                    </a>
-                    <a href="<?= $BASE_URL ?>/create.php" class="nav-link active" id="home-link">
-                        Add Contact
-                    </a>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+                <a class="navbar-brand" href="<?= $BASE_URL ?>/index.php">
+                    <img src="<?= $BASE_URL ?>/img/logo.svg" alt="Agenda">
+                </a>
+                <div>
+                    <div class="navbar-nav">
+                        <a href="<?= $BASE_URL ?>/index.php" class="nav-link active" id="home-link">
+                            Agenda
+                        </a>
+                        <a href="<?= $BASE_URL ?>/create.php" class="nav-link active" id="home-link">
+                            Add Contact
+                        </a>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
     </header>
